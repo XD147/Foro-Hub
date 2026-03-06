fcdba57c788b83abe0f75e60fce629a8c45d9967
GICAMUBE <gicamube@gmail.com>
2026-03-06 16:17:01 -0300

    ActualizaMigrations

--- src/main/resources/db/migration/V2__create-table-topico.sql
+++ src/main/resources/db/migration/V2__create-table-topico.sql
@@ -1,9 +1,15 @@
-create table usuarios(
+create table topicos(
 
     id bigint not null auto_increment,
-    login varchar(100) not null,
-    password varchar(255) not null,
+
+    usuario_id bigint not null,
+    curso varchar(100) not null,
+    titulo varchar(255) not null,
+    mensaje varchar(255) not null,
+    fecha datetime not null,
+    status varchar(100) not null,
 
     primary key(id)
 
+    constraint fk_topicos_usuario_id foreign key(usuario_id) references usuarios(id)
 );
